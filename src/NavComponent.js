77de36c23837f25a34f37cfbaa29803901a447a4
Joel <topsupp@hotmail.com>
2019-01-15 17:58:54 +0000

    updated single hero component styling

--- src/NavComponent.js
+++ src/NavComponent.js
@@ -12,6 +12,7 @@ import Heroes from './HeroesComponent.js';
 import Battlegrounds from './BattlegroundsComponent.js';
 import Register from './RegisterComponent.js';
 import Login from './LoginComponent.js';
+import SingleHero from './SingleHeroComponent.js';
 
 class NavComponent extends Component {
   render() {
@@ -21,14 +22,14 @@ class NavComponent extends Component {
 				<ul>
 					<li><Link to="/">Home</Link></li>
 					<li><Link to="/heroes">Heroes</Link></li>
-					<li><Link to="/battlegrounds">Battlegrounds</Link></li>
+					<li><Link to="/hero">Hero</Link></li>
 					<li><Link to="/register">Register</Link></li>
 					<li><Link to="/login">Login</Link></li>
 				</ul>
 
 				<Route exact path="/" component={ Homepage } />
 				<Route path="/heroes" component={ Heroes } />
-				<Route path="/battlegrounds" component={ Battlegrounds } />
+				<Route path="/hero" component={ SingleHero } />
 				<Route path="/register" component={ Register } />
 				<Route path="/login" component={ Login } />
 			</nav>
